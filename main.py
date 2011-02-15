b929a12bc1ddb16771bc7592043c3ccf85cfd807
mybeky <mybeky@gmail.com>
2011-02-15 12:02:37 +0800

    scrobble method done !

--- main.py
+++ main.py
@@ -1,29 +1,70 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+import urllib, urllib2, time
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+from google.appengine.api import taskqueue
+
+import xml.etree.ElementTree as ET
+import md5
+import logging
+
+url = 'http://www.xiami.com/widget/xml-dynamic/uid/435580/id/435580/type/shuffle'
+
+def scrobble(title, artist, ts):
+    url = 'http://ws.audioscrobbler.com/2.0/'
+    api_key = 'ebd2a16df68496b34da760f2eff69d64'
+    secret = 'c36527186f7aca092a8795fc2047e42b'
+    sk = '827f381b9154c79b81a9d345f6211b2b'
+
+    params = {
+        'track[0]': title,
+        'artist[0]': artist,
+        'timestamp[0]': ts,
+        'api_key': 'ebd2a16df68496b34da760f2eff69d64',
+        'method': 'track.scrobble',
+        'sk': sk
+    } 
+    s = ''.join([x + str(params[x]) for x in sorted(data.keys())])
+    s += secret
+    params['api_sig'] = md5.new(s).hexdigest()
+
+    result = urlfetch.fetch(url=url, 
+                            payload=urllib.urlencode(params), 
+                            method=urlfetch.POST).content
+
+
+class Track(db.Model):
+    title = db.StringProperty(required=True)
+    artist = db.StringProperty(required=True)
+    album = db.StringProperty()
+    source = db.StringProperty()
+    ts = db.DateTimeProperty()
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        res = urllib.urlopen(url).read()
+        xml = ET.XML(res)
+        response = '<ul>'
+        for track in xml.findall('trackList/track'):
+            title = track.find('song_name').text
+            artist = track.find('artist_name').text
+            response += '<li>%s - %s</li>' % (title, artist)
+            scrobbler.submit(artist, title,
+                             int(time.mktime(time.localtime())),
+                             rating='L',
+                             length=335,
+                             autoflush=True)
+            break
+        #scrobbler.flush()
+        response += '</ul>'
+        self.response.out.write(response)
 
 
 def main():
+    logging.getLogger().setLevel(logging.DEBUG)
     application = webapp.WSGIApplication([('/', MainHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
